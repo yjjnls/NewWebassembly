33a4aed3f3f1dddc6de26703e5b8f02627cafcf4
yjjnls <x-jj@foxmail.com>
2018-09-12 09:32:11 +0800

    update

--- opencv.test/bind.cc
+++ opencv.test/bind.cc
@@ -6,188 +6,27 @@
 #include "../../example.h"
 #include <map>
 
-using namespace emscripten::internal;
-
-/*
-map<class_name, table(map)>
-                  ↑
-                map<function_name, function_map(hash_map)>
-                                            ↑
-                                    <arg_num, function_info>
-*/
-struct function_info
+class Addon
 {
-    function_info(GenericFunction function,
-                  const std::string &name,
-                  unsigned argcount = 0,
-                  TYPEID argtypes[] = nullptr)
-        : fun(function)
-        , funName(name)
-        , argCount(argcount)
-        , argTypes(nullptr)
-    // , invoker(nullptr)
-    // , setter(nullptr)
-    {
-        argTypes = new TYPEID[argCount];
-        // memset(argTypes,0,sizeof(TYPEID)*argCount);
-        for (int i = 0; i < argCount; ++i) {
-            argTypes[i] = argtypes[i];
-        }
-    }
+ public:
+    Addon() {}
+    ~Addon() {}
+    static void Init(napi_env env, napi_value exports);
+    static void Destructor(napi_env env, void *nativeObject, void *finalize_hint);
 
-    ~function_info()
-    {
-        if (argTypes != nullptr) {
-            delete[] argTypes;
-            argTypes = nullptr;
-        }
-    }
+ private:
+    static napi_value New(napi_env env, napi_callback_info info);
+    static napi_value create(napi_env env, napi_callback_info info);
+    static napi_value roi(napi_env env, napi_callback_info info);
+    static napi_value get_rows(napi_env env, napi_callback_info info);
+    static napi_value set_rows(napi_env env, napi_callback_info info);
+    static napi_value get_data32S(napi_env env, napi_callback_info info);
 
+    static napi_ref constructor;
 
-    GenericFunction fun;
-    std::string funName;
-
-    unsigned argCount;
-
-    // std::string invokerSignature;
-    // GenericFunction invoker;
-
-    // GenericFunction setter;
-    TYPEID *argTypes;
+    napi_env env_;
+    napi_ref wrapper_;
 };
-typedef std::map<int, function_info *> function_map;
-std::map<std::string /*function name*/, function_map *> table;
-
-extern "C" {
-void _embind_register_class(
-    TYPEID classType,         // rtti info of this class(Mat)
-    TYPEID pointerType,       // rtti info of Mat
-    TYPEID constPointerType,  // rtti info of Mat
-    TYPEID baseClassType,     // rtti info of base class(nullptr)
-    const char *getActualTypeSignature,
-    GenericFunction getActualType,  // pointer of function to get the rtti info of this class(Mat)
-    const char *upcastSignature,
-    GenericFunction upcast,  // pointer of function to upcast this class to base class(nullptr)
-    const char *downcastSignature,
-    GenericFunction downcast,  // pointer of function to downcast base class to this class(nullptr)
-    const char *className,     // class name("Mat")
-    const char *destructorSignature,
-    GenericFunction destructor)  // pointer of delete function
-{
-
-    // printf("---------------------------\n");
-    // printf("%s\n%s\n%s\n%p\n%s\n%p\n%s\n%p\n%s\n%p\n%s\n%s\n%p\n", ((std::type_info *)classType)->name(), ((std::type_info *)pointerType)->name(), ((std::type_info *)constPointerType)->name(), baseClassType, getActualTypeSignature, getActualType, upcastSignature, upcast, downcastSignature, downcast, className, destructorSignature, destructor);
-    // printf("---------------------------\n");
-    function_map *s = new function_map;
-    (*s)[0] = new function_info(destructor, "destructor");
-    table["destructor"] = s;
-}
-// GenericFunction construct = nullptr;
-void _embind_register_class_constructor(
-    TYPEID classType,
-    unsigned argCount,
-    const TYPEID argTypes[],
-    const char *invokerSignature,
-    GenericFunction invoker,
-    GenericFunction constructor)
-{
-    // printf("--------------register constructor: invokerSignature %s-------------\n", invokerSignature);
-    // printf("argCount: %d\n", argCount);
-    // for (int i = 0; i < argCount; ++i) {
-    //     printf("%s\n", ((std::type_info *)argTypes[i])->name());
-    // }
-    if (table["constructor"] == nullptr) {
-        table["constructor"] = new function_map;
-    }
-    function_map *map = table["constructor"];
-    function_info *info = new function_info(constructor, "constructor");
-    // info->argCount=argCount;
-    // info->argTypes=argTypes;
-    (*map)[argCount - 1] = info;
-
-    // construct = constructor;
-    // printf("~~~~ constructor: %p\n",construct);
-    // printf("%s\n", (typeid(std::string)).name());
-}
-
-void _embind_register_class_function(
-    TYPEID classType,
-    const char *methodName,
-    unsigned argCount,
-    const TYPEID argTypes[],
-    const char *invokerSignature,
-    GenericFunction invoker,
-    void *context,
-    unsigned isPureVirtual)
-{
-    std::string fun_name = std::string(methodName);
-
-    // printf("function name: %s \n", fun_name.c_str());
-    // printf("argCount: %d\n", argCount);
-    // for (int i = 0; i < argCount; ++i) {
-    //     printf("%s\n", ((std::type_info *)argTypes[i])->name());
-    // }
-
-    if (table[fun_name] == nullptr) {
-        table[fun_name] = new function_map;
-    }
-    function_map *map = table[fun_name];
-    function_info *info = new function_info(context, fun_name);
-    (*map)[argCount - 2] = info;  // arg[0]:return_type arg[1]:class_type arg[2...]:function_args
-}
-
-void _embind_register_class_property(
-    TYPEID classType,
-    const char *fieldName,
-    TYPEID getterReturnType,
-    const char *getterSignature,
-    GenericFunction getter,
-    void *getterContext,
-    TYPEID setterArgumentType,
-    const char *setterSignature,
-    GenericFunction setter,
-    void *setterContext)
-{
-    std::string property_name = std::string(fieldName);
-    if (table[property_name] == nullptr) {
-        table[property_name] = new function_map;
-    }
-    function_map *map = table[property_name];
-    function_info *info_getter = new function_info(getterContext, property_name + ".getter");
-    function_info *info_setter = new function_info(setterContext, property_name + ".setter");
-    (*map)[0] = info_getter;
-    (*map)[1] = info_setter;
-}
-
-void _embind_register_class_class_function(
-    TYPEID classType,
-    const char *methodName,
-    unsigned argCount,
-    const TYPEID argTypes[],
-    const char *invokerSignature,
-    GenericFunction invoker,
-    GenericFunction method)
-{
-    std::string fun_name = std::string(methodName);
-    if (table[fun_name] == nullptr) {
-        table[fun_name] = new function_map;
-    }
-    function_map *map = table[fun_name];
-    function_info *info = new function_info(method, fun_name);
-    (*map)[argCount - 1] = info;
-}
-
-void _embind_register_class_class_property(
-    TYPEID classType,
-    const char *fieldName,
-    TYPEID fieldType,
-    const void *fieldContext,
-    const char *getterSignature,
-    GenericFunction getter,
-    const char *setterSignature,
-    GenericFunction setter) {}
-}
-
 template <typename ClassType>
 ClassType *constructor_factory(napi_env env, size_t argc, napi_value *args)
 {
