d9e23bd9958eadcc3e1b117973d2cd01ab2f0e10
yjjnls <x-jj@foxmail.com>
2018-09-17 19:37:54 +0800

    update

--- n-api/example.js
+++ n-api/example.js
@@ -2,10 +2,39 @@
 const bindingPath = require.resolve(`./build/Release/plugin`);
 const Module = require(bindingPath);
 
-class MyClass{
-    constructor(x,y){
-        this.target=new Module.MyClass(x,y);
+class MyClass {
+    constructor(x, y) {
+        if (!!x && !!y)
+            this.target = new Module.MyClass(x, y);
+        else if (!!x)
+            this.target = new Module.MyClass(x);
+        else
+            this.target = new Module.MyClass();
+
+        this.x = this.target.x;
+    }
+
+    oper(val) {
+        return this.target.oper(val.target);
+    }
+    incrementX(val) {
+        if (!!val)
+            this.target.incrementX(val);
+        else
+            this.target.incrementX();
+    }
+    static getStringFromInstance(val) {
+        return Module.getStringFromInstance(val.target);
+    }
+    delete() {
+        this.target = null;
+        this.x = null;
+        global.gc();
     }
 }
 
-module.exports = MyClass;
+module.exports = { "MyClass": MyClass };
+
+
+// test
+
--- n-api/test.js
+++ n-api/test.js
@@ -1,12 +1,30 @@
-'use strict';
-const bindingPath = require.resolve(`./build/Release/plugin`);
-const Module = { "MyClass": require(bindingPath) };
-// const Module = require(bindingPath);
+// 'use strict';
+// const bindingPath = require.resolve(`./build/Release/plugin`);
+// const Module = { "MyClass": require(bindingPath) };
+// // const Module = require(bindingPath);
 
 
-// var instance = new Module.MyClass(10, "hello");
-var instance = Module.MyClass.createObject(10, "hello");
+// // var instance = new Module.MyClass(10, "hello");
+// var instance = Module.MyClass.createObject(10, "hello");
 
+// instance.oper(instance);
+// instance.incrementX();
+// console.log('instance.x: ' + instance.x);
+// instance.x = 20;
+// console.log('instance.x: ' + instance.x);
+// instance.incrementX(3);
+// console.log('instance.x: ' + instance.x);
+
+// console.log('instance.string: ' + Module.MyClass.getStringFromInstance(instance));
+// // // console.log('instance.string: ' + Module.getStringFromInstance(instance));
+// instance.delete();
+// // instance = null;
+// // global.gc();
+
+const Module = require('./example.js');
+
+
+var instance = new Module.MyClass(10, "hello");
 instance.oper(instance);
 instance.incrementX();
 console.log('instance.x: ' + instance.x);
@@ -16,7 +34,4 @@ instance.incrementX(3);
 console.log('instance.x: ' + instance.x);
 
 console.log('instance.string: ' + Module.MyClass.getStringFromInstance(instance));
-// // console.log('instance.string: ' + Module.getStringFromInstance(instance));
 instance.delete();
-// instance = null;
-// global.gc();
